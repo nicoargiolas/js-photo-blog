a349f838e730d71b28d14a7ab8c2ccc448853e7d
nicoargiolas <argiolas.nicola00@gmail.com>
2025-01-24 15:04:34 +0100

    Aggiunta EventListener sui post e commenti js

--- script/script.js
+++ script/script.js
@@ -16,15 +16,19 @@
 // (Dovreste sapere a questo punto cosa e come prendere da GFonts…)
 
 // Collegamento a elementi del DOM
-const postsHTML = document.querySelector('.posts-container');
+const postsContainerHTML = document.querySelector('.posts-container');
+const imgBoxHTML = document.querySelector('.large-img-box');
 
+// Chiamata a API per ottenere l'array di oggetti per generare i post
 axios.get('https://lanciweb.github.io/demo/api/pictures/')
     .then(response => {
-        const posts = response.data;
+        // Salvo la risposta in una variabile
+        let posts = response.data;
         console.table(posts);
+        // Creo un ciclo per stampare i post in pagina utilizzando i riferimenti alle proprietà degli oggetti nell'array dato dall'API
         let counter = posts.length;
         for (let i = 0; i < counter; i++) {
-            postsHTML.innerHTML += `
+            postsContainerHTML.innerHTML += `
             <div class="post">
                 <img src="./img/pin.svg" alt="" class="pin">
                 <div class="img-box">
@@ -34,7 +38,22 @@ axios.get('https://lanciweb.github.io/demo/api/pictures/')
                     <p class="data"> ${posts[i].date}</p>
                     <p class="titolo"> ${posts[i].title.toUpperCase()} </p>
                 </div>
-            </div>`
+            </div>`;
+        }
+    // Una volta scritti gli elementi in pagina prendo i post salvandoli in una NodeList
+    const postHTML = document.querySelectorAll('.post');
+        // Aggiungo un EventListener a tutti i post utilizzando un ciclo
+        for (let i = 0; i < counter; i++) {
+            postHTML[i].addEventListener("click", () => {
+                // Al click su un post verrà mostrata in pagina l'immagine corrispondente ingrandita sempre usando i riferimenti alle proprietà degli oggetti
+                imgBoxHTML.classList.remove("hide");
+                imgBoxHTML.innerHTML = `<img src="${posts[i].url}" alt="Immagine di ${posts[i].title}">`;
+            });
+                // Creo un altro EventListener che mi permette con un altro click di nascondere l'immagine ingrandita e poter di nuovo scorrere tra i post
+            imgBoxHTML.addEventListener("click", () => {
+                imgBoxHTML.classList.add("hide");
+                imgBoxHTML.innerHTML = "";
+            })
         }
     })
     .catch(error => {
